5931291ab0b90ecc0f84d64b897dd1104982e842
Adekunle Adewoye <kunlexy12@outlook.com>
2022-12-20 21:06:10 +0100

    Initial commit

---
+++ .history/index_20221220210253.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// // res.send('Hello World!');
+// });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Server running on port ${port}'));
